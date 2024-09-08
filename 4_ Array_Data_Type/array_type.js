f663761a55609b2c21b99301eb3076d070662195
Tushar-Hossain <tusharhossain455@gmail.com>
2024-09-09 01:06:15 +0600

    array data type

---
+++ 4_ Array_Data_Type/array_type.js
@@ -0,0 +1,9 @@
+// let userName: string[];
+// let userNames: number[];
+// let userName: (string | number)[];
+var userName;
+userName = ["Tusahr", "Nadia", "Shahin", 101];
+// userNames = [101, 102, 103];
+console.log(userName[0]);
+console.log(userName[3]);
+// console.log(userNames[0]);

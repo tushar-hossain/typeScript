3b956725820861aec9a441bcc3e4f2c8fb31b86d
Tushar-Hossain <tusharhossain455@gmail.com>
2024-09-09 00:23:23 +0600

    Union Data Type

---
+++ 3_union_data_type/union_type.js
@@ -0,0 +1,9 @@
+// let userId: number | string | boolean;
+// userId = 101;
+// userId = "101";
+// userId = true;
+function display(userId) {
+    console.log(userId);
+}
+display(101);
+display("101");
---
+++ 3_union_data_type/union_type.ts
@@ -0,0 +1,12 @@
+// let userId: number | string | boolean;
+
+// userId = 101;
+// userId = "101";
+// userId = true;
+
+function display(userId: number | string) {
+  console.log(userId);
+}
+
+display(101);
+display("101");

bf9ba78327916165c746235869028c01c74746d9
Tushar-Hossain <tusharhossain455@gmail.com>
2024-09-09 01:18:15 +0600

    tuple data type

---
+++ 5 Tuple Data Type/tuple.js
@@ -0,0 +1,6 @@
+// tuple mixed data type  => 
+var user;
+user = [101, "tushar"];
+user.push(102, "nadia");
+console.log(user);
+console.log(user[0]);
---
+++ 5 Tuple Data Type/tuple.ts
@@ -0,0 +1,10 @@
+// tuple mixed data type  => 
+
+let user: [number, string];
+
+user = [101, "tushar"];
+
+user.push(102, "nadia");
+
+console.log(user);
+console.log(user[0]);

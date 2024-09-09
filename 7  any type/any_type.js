cc876f987b9625f8ab4e404b1fc069d1817af7f1
Tushar-Hossain <tusharhossain455@gmail.com>
2024-09-09 17:03:19 +0600

    Any Data Type

---
+++ 7  any type/any_type.js
@@ -0,0 +1,7 @@
+// Any Data Type
+var userName;
+userName = "Tushar";
+userName = 101;
+userName = true;
+userName = [101, 102, "Tushar"];
+console.log(userName);
---
+++ 7  any type/any_type.ts
@@ -0,0 +1,10 @@
+// Any Data Type
+
+let userName: any;
+
+userName = "Tushar";
+userName = 101;
+userName = true;
+userName = [101, 102, "Tushar"];
+
+console.log(userName);
